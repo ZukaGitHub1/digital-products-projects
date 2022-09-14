b9c909f4331a6b79f87551b8d7d65e2c5fd10134
Vaniko Akopashvili <49734341+VanoAkofashvili@users.noreply.github.com>
2022-09-14 21:44:36 +0400

    test

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import ProductLaptop from './components/categroiesFilter/ProductLaptop';
 import Main from './pages/home/Main';
 
 
+function CategoryPage() {
+  const params = useParams();
+ 
+  useEffect(() => {
+    fetch('https://academy-products.herokuapp.com/api/products/category/${params.id}')
+  }, [])
+
+  return;
+}
+
+
 function App() {
   return <div className="App">
     <Routes>
       <Route  path='/' element={<Main/>}/>
-      <Route path='/category/1' element={<ProductPhone/>} />
-      <Route path='/category/2' element={<ProductLaptop/>} />
+      {/* <Route path='/category/1' element={<ProductPhone />} />
+      <Route path='/category/2' element={<ProductLaptop />} /> */}
+      <Route path="/category/:id" element={<CategoryPage />} />
       <Route path='*' element={<div>page not found</div>}/>
     </Routes>
     </div>
